3fb66feee49a0d4491eac5322e6c9284349008c0
aacotroneo <aacotroneo@gmail.com>
2015-02-02 21:48:47 +0000

    initial laravel package setup

---
+++ src/Aacotroneo/LaravelSaml2/LaravelSaml2ServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Aacotroneo\LaravelSaml2;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelSaml2ServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('aacotroneo/laravel-saml2');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}

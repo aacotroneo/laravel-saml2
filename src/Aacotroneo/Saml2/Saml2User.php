d5debc353e33171132c26f0f8deb96c61a8b3b94
Robert Boes <robert@hihaho.com>
2017-07-13 12:56:43 +0200

    Added getAttribute function and functions to parse the SAML attributes (and add them to the Saml2User)

--- src/Aacotroneo/Saml2/Saml2User.php
+++ src/Aacotroneo/Saml2/Saml2User.php
@@ -41,6 +41,18 @@ class Saml2User
     }
 
     /**
+     * Returns the requested SAML attribute
+     *
+     * @param string $name The requested attribute of the user.
+     * @return array|null Requested SAML attribute ($name).
+     */
+    function getAttribute($name) {
+        $auth = $this->auth;
+
+        return $auth->getAttribute($name);
+    }
+
+    /**
      * @return string the saml assertion processed this request
      */
     function getRawSamlAssertion()
@@ -57,6 +69,35 @@ class Saml2User
         if ($relayState && $url->full() != $relayState) {
 
             return $relayState;
+        }
+    }
+
+    /**
+     * Parses a SAML property and adds this property to this user or returns the value
+     *
+     * @param string $samlAttribute
+     * @param string $propertyName
+     * @return array|null
+     */
+    function parseUserAttribute($samlAttribute = null, $propertyName = null) {
+        if(empty($samlAttribute)) {
+            return null;
+        }
+        if(empty($propertyName)) {
+            return $this->getAttribute($samlAttribute);
+        }
+
+        return $this->{$propertyName} = $this->getAttribute($samlAttribute);
+    }
+
+    /**
+     * Parse the saml attributes and adds it to this user
+     *
+     * @param array $attributes Array of properties which need to be parsed, like this ['email' => 'urn:oid:0.9.2342.19200300.100.1.3']
+     */
+    function parseAttributes($attributes = array()) {
+        foreach($attributes as $propertyName => $samlAttribute) {
+            $this->parseUserAttribute($samlAttribute, $propertyName);
         }
     }
 

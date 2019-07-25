34fe227c2683dd8ed2a2b439731bb5a1755841b7
darynmitchell <daryn.mitchell@gmail.com>
2019-07-25 14:50:06 +0300

    Make it easy to subclass Saml2Controller
    It is normal to want to extend Saml2Controller,
    notably to pass your app's own "redirect URL"
    to the Saml2Auth login() call, so that the
    RelayState will be set accordingly.
    
    This commit makes it easy to do so by adding
    an optional config value that lets you specify the controller.
    By default, with no value provided, the routes will
    be configured as before, with the default Saml2Controller.
    
    Updates the Readme with example of how to do it.

--- src/routes.php
+++ src/routes.php
@@ -3,29 +3,31 @@
 Route::middleware(config('saml2_settings.routesMiddleware'))
 ->prefix(config('saml2_settings.routesPrefix').'/')->group(function() {
     Route::prefix('{idpName}')->group(function() {
+	$saml2_controller = config('saml2_settings.saml2_controller', 'Aacotroneo\Saml2\Http\Controllers\Saml2Controller');
+
         Route::get('/logout', array(
             'as' => 'saml2_logout',
-            'uses' => 'Aacotroneo\Saml2\Http\Controllers\Saml2Controller@logout',
+            'uses' => $saml2_controller.'@logout',
         ));
 
         Route::get('/login', array(
             'as' => 'saml2_login',
-            'uses' => 'Aacotroneo\Saml2\Http\Controllers\Saml2Controller@login',
+            'uses' => $saml2_controller.'@login',
         ));
 
         Route::get('/metadata', array(
             'as' => 'saml2_metadata',
-            'uses' => 'Aacotroneo\Saml2\Http\Controllers\Saml2Controller@metadata',
+            'uses' => $saml2_controller.'@metadata',
         ));
 
         Route::post('/acs', array(
             'as' => 'saml2_acs',
-            'uses' => 'Aacotroneo\Saml2\Http\Controllers\Saml2Controller@acs',
+            'uses' => $saml2_controller.'@acs',
         ));
 
         Route::get('/sls', array(
             'as' => 'saml2_sls',
-            'uses' => 'Aacotroneo\Saml2\Http\Controllers\Saml2Controller@sls',
+            'uses' => $saml2_controller.'@sls',
         ));
     });
 });
